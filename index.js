e37fbdd00752e871316b60f58062c9783093fa48
topstreamapp <contact@topstream.pro>
2019-03-17 02:09:33 +0300

    0.1.0
    First test version

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('*', (req, res) => {
-    res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>')
-    res.write('<h2>Go to <a href="/about">/about</a></h2>')
-    res.end()
-})
+const RAMDB = {
+    twitchChannels: [],
+    queries: 0,
+    errors: 0
+};
 
-module.exports = app
+app.get('/', () => {
+    res.redirect('https://github.com/topstreamapp/mushroom');
+});
+
+app.get('/getStats', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(RAMDB);
+});
+
+app.get('/addTwitchChannel', (req, res) => {
+    RAMDB.queries++;
+
+    res.set('Access-Control-Allow-Origin', '*');
+
+    const result = {
+        success: true
+    };
+
+    try {
+        const channelData = {
+            username: req.query.username,
+            id: parseInt(req.query.id),
+            partner: req.query.partner,
+            followers: parseInt(req.query.followers)
+        };
+
+        for (let exChannelData of RAMDB.twitchChannels) {
+            if (exChannelData.id === channelData.id) {
+                result.success = false;
+                result.msg = 'Channel already in DB!';
+                res.json(result);
+                return false;
+            }
+        }
+
+        RAMDB.twitchChannels.push(channelData);
+
+        if (RAMDB.twitchChannels.length > 4096)
+            RAMDB.twitchChannels.splice(0, 1);
+
+        res.json(result);
+    } catch (err) {
+        RAMDB.errors++;
+        result.success = false;
+        result.msg = 'Unable to add Twitch channel! Check your query.';
+        res.json(result);
+    }
+});
+
+module.exports = app;
